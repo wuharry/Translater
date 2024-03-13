6377c683705b08eefabc7352eddd8f5221f4e58e
wuharry <whw880218cool@gmail.com>
2024-03-13 15:50:39 +0800

    fix:fix respone  type error

--- Translater/src/App.tsx
+++ Translater/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { ApiRequest } from "./api/api";
-
+import { TranslateResponse } from "./types/ApiRespone";
 import "./App.css";
 
 function App() {
@@ -19,18 +19,22 @@ function App() {
       providers: "amazon,google,ibm,microsoft",
       text: word,
       source_language: "en",
-      target_language: "zh",
+      target_language: "zh-TW",
       fallback_providers: "",
     };
 
-    const res = await ApiRequest("post", translateUrl, data);
-    console.log();
+    const res = (await ApiRequest(
+      "post",
+      translateUrl,
+      data
+    )) as TranslateResponse;
+    console.log(res);
 
     setWordList([
       ...wordList,
       {
         untranslateWord: word,
-        translatedWord: res.data.amazon.text,
+        translatedWord: res?.amazon?.text,
       },
     ]);
     setWord(" ");
@@ -39,16 +43,14 @@ function App() {
   return (
     <>
       <div></div>
-      <h1>Vite + React</h1>
+      <h1>英文單字翻譯朗讀</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>please input the word</p>
       </div>
-      <div className="">
+      <div className="w-full flex  justify-around">
         <label>
           Word:
           <input
@@ -69,13 +71,29 @@ function App() {
           Add
         </button>
       </div>
-      <div>
+      <div className="">
         <h2 className="text-xl font-bold mb-4">Word Table</h2>
-        <table className="table-auto border border-collapse border-black">
+        <table className=" w-full flex justify-center items-center table-auto border border-collapse border-black">
           <thead>
             <tr>
               <th className="border border-black px-4 py-2">Word</th>
               <th className="border border-black px-4 py-2">中文</th>
+              <th>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+                  />
+                </svg>
+              </th>
             </tr>
           </thead>
           <tbody>
